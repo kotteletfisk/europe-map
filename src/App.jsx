b96a964dd8b7443f62c67dee4716a7fc8ff12ca4
kotteletfisk <ostekugle@gmail.com>
2023-12-01 12:56:35 +0100

    active actions

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SvgComponent from './SvgComponent'
-// some comment
+
 function App() {
 
   return (
